9914a9966fedf57399cdad19e86e883c5010edc6
Yugant Sidar <sidaryugant97@gmail.com>
2025-06-15 20:58:05 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,11 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
-    },
-  },
   server: {
     // proxy: {
     //   "/api": "http://localhost:3000",
