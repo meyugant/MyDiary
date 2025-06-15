10737f7c1876d568898fcd4ee06a3332e9934f1a
Yugant Sidar <sidaryugant97@gmail.com>
2025-06-15 21:27:00 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -422,7 +422,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:3000/auth/google/mydiary",
+      callbackURL:`${process.env.API_BASE_URL}/auth/google/mydiary`,
       userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
     },
     async (accessToken, refreshToken, profile, cb) => {
