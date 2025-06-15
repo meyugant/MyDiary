666eb70afd2973343025e14a7d8cee33f0fb84c4
Yugant Sidar <sidaryugant97@gmail.com>
2025-06-15 23:01:48 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -52,7 +52,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
     origin: ["http://localhost:5173",
-        "https://my-diary-neon.vercel.app",
+        "https://mydiary-ten.vercel.app",
         ],
     methods: "GET,POST,DELETE,PUT",
     credentials: true, // important for cookies/session
