38ed2cd3f662f874726e3c380b344f608eb9f5df
meyugant <sidaryugant97@gmail.com>
2026-07-01 16:02:59 +0530

    minor bug fix

--- backend/index.js
+++ backend/index.js
@@ -27,20 +27,20 @@ cloudinary.config({
 
 console.log(cloudinary.config());
 
-const db = new pg.Client({
-  user: process.env.PG_USER,
-  host: process.env.PG_HOST,
-  database: process.env.PG_DATABASE,
-  password: process.env.PG_PASSWORD,
-  port: process.env.PG_PORT,
-});
-
 // const db = new pg.Client({
-//   connectionString: process.env.DATABASE_URL,
-//   ssl: {
-//     rejectUnauthorized: false,
-//   },
+//   user: process.env.PG_USER,
+//   host: process.env.PG_HOST,
+//   database: process.env.PG_DATABASE,
+//   password: process.env.PG_PASSWORD,
+//   port: process.env.PG_PORT,
 // });
+
+const db = new pg.Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 
 const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
